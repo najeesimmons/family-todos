61938cc7164afb7eccd9afc7a0dd6689f4630214
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-23 17:34:34 -0400

    map function working to display items in TodoDisplay

--- src/components/ToDoDisplay.js
+++ src/components/ToDoDisplay.js
@@ -9,7 +9,6 @@ const TodoDisplay = (props) => {
       <ul>
         {props.todos.map((item) => (
           <TodoItem key={item.id} id={item.id} text={item.text} />
-  
         ))}
       </ul>
     </div>
