9409063e97ac9dc006b8bfc76144ee0e4984661b
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-23 15:20:51 -0400

    created and rendering ToDoInput and Button components

---
+++ src/components/TodoInput.js
@@ -0,0 +1,12 @@
+import Button from "./UI/Button";
+
+const TodoInput = () => {
+  return (
+    <div>
+      <h2>New Task</h2>
+      <Button />
+    </div>
+  );
+};
+
+export default TodoInput;
---
+++ src/components/UI/Button.js
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <div>
+        <button>Add</button>
+        </div>
+    )
+}
+
+export default Button
