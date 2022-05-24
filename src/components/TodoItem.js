70947025affea4bd0ad5bf7cc5d80515fb01f677
najeesimmons <najee.bryant.simmons@gmail.com>
2022-05-23 22:03:39 -0400

    added useffect to TodoItem to keep track of ischecked in real time

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+
 const TodoItem = (props) => {
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${isChecked}`)
+  }, [isChecked])
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  }
+
   return (
     <div>
       <p>{props.text}</p>
-      <input type="checkbox"></input>
+      <input type="checkbox" checked={isChecked} onChange={handleOnChange}></input>
+      <h5>Test:Above checkbox is {isChecked ? "checked" : "un-checked"}.</h5>
     </div>
   );
 };
