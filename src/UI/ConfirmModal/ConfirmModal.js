198920106528789935dffa0ba660330a28e5fca7
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-03 14:15:48 -0400

    added onclock to ConfirmModal backdrop so that clicking it will deactivate modal display status

--- src/UI/ConfirmModal/ConfirmModal.js
+++ src/UI/ConfirmModal/ConfirmModal.js
@@ -1,26 +1,30 @@
-import style from './ConfirmModal.module.css'
+import style from "./ConfirmModal.module.css";
+import Card from "../Card/Card";
 
-const ConfirmModal = props => {
+const ConfirmModal = (props) => {
+  const finalDelete = () => {
+    props.onDeleteTodos(props.id);
+  };
+  return (
+    <div>
+      <div className={style.backdrop} onClick={props.confirmDelete}>
+        <Card className={style.modal}>
+          <header className={style.header}>
+            <h2>Confirm Deletion</h2>
+          </header>
+          <div className={style.content}>
+            Are you sure you want to delete this item?
+          </div>
+          <footer className={style.actions}>
+            <button onClick={finalDelete} id={props.id}>
+              Yes
+            </button>
+            <button onClick={props.confirmDelete}>No</button>
+          </footer>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-    const finalDelete = () => {
-        props.onDeleteTodos(props.id)
-    }
-    return (
-        <div>
-        <div className={style.backdrop}>
-            <div className={style.modal}>
-            <header>
-                <h1>{props.title}</h1>
-            </header>
-            <div>{props.message}</div>
-            <footer>
-                <button onClick={props.confirmDelete}></button>
-                <button onClick={finalDelete} id={props.id}></button>
-            </footer>
-            </div>
-        </div>
-        </div>
-    )
-}
-
-export default ConfirmModal
+export default ConfirmModal;
