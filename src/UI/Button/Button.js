7ffa127926479265a0bff99124e2d59d2fb10a94
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-06 10:42:03 -0400

    fixed click issue

--- src/UI/Button/Button.js
+++ src/UI/Button/Button.js
@@ -6,7 +6,7 @@ const Button = props => {
   let color = props.green ? styles.green : props.red ? styles.red : null;
 
   return (
-      <button type={props.type || 'button'} className={`${styles.button} ${color}`} Click={props.onClick}>
+      <button type={props.type || 'button'} className={`${styles.button} ${color}`} onClick={props.onClick}>
       {/* <button type={props.type || 'button'} className={[color, styles.button]} Click={props.onClick}> */}
         {props.children}
       </button>
